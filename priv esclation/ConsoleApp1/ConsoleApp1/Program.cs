7a3dae7f45cc20cc0ef4418027f138d045392bfb
Ahmed Shams <96126679+0xSh3RlocK@users.noreply.github.com>
2022-08-12 22:47:37 +0200

    Add files via upload

---
+++ priv esclation/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceProcess;
+using Microsoft.Win32;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //HKLM\SYSTEM\CurrentControlSet\Services  
+
+            //Registry.LocalMachine.OpenSubKey(@"SYSTEM\CurrentControlSet\Services");
+            ServiceController[] scs = ServiceController.GetServices();
+
+            foreach (ServiceController controller in scs)
+            {
+                RegistryKey rkey =  Registry.LocalMachine.OpenSubKey(@"SYSTEM\CurrentControlSet\Services"  +controller.ServiceName);
+                
+                
+                string Paht = rkey.GetValue("ImagePath").ToString();
+
+                if (Paht[0] != '"' && !Paht.Contains("system32") && !Paht.Contains("System32"))
+                {
+                    Console.WriteLine(controller.ServiceName);
+                    Console.WriteLine(Paht);
+                }
+            }
+           
+            Console.ReadKey();
+
+        }
+    }
+}
